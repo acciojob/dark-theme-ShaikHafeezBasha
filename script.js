d5dbbdb0a8158fb025ea5bc9d7b86891eed21022
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-08-16 19:31:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
-
+let btn=document.getElementById('swap');
+btn.addEventListener('click',()=>{
+	let main=document.getElementById('app')
+	if(main.className=='night'){
+		main.className='button_night'
+	}
+	else{
+		main.className='night'
+	}
+})
